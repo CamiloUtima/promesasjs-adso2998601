5133449594757251d27ea9b42c0806ba41f7b6f2
CamiloUtima <juancamiloutima@gmail.com>
2025-05-21 14:15:22 -0500

    Configuracion de promesas completo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 //Funcion con async
 
+function descargarArchivo(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("📁 Archivo descargado con exito!");
+        }, 5000);
+    })
+}
+
 async function ejecutar() {
     try {
         console.log("⏳ Descarga en proceso...")
-        const respuesta = await fetch
+        const respuesta = await descargarArchivo()
+        console.log(respuesta)
     } catch (error) {
-        
+        console.error("❌error al descargar")
     }
 }
+
+ejecutar()
+
