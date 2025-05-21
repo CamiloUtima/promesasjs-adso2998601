30b8c3f70dce4e7461c430241534af24c37b0253
CamiloUtima <juancamiloutima@gmail.com>
2025-05-21 15:01:43 -0500

    Ejercicio2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-function descargarArchivoConError(hayError) {
+// ejercicio2
+// crear una promesa que se rechace después de 3 segundos si un numero aleatorio es menor que 0.5, si no que se resuelva
+
+function promesaRechazada() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (hayError) {
-                reject("❌ Error: Hubo un problema al descargar el archivo.");
+            const numeroAleatorio = Math.random();
+            console.log("🔢 Número aleatorio generado:", numeroAleatorio);
+
+            if (numeroAleatorio < 0.5) {
+                reject("❌ Error: El número aleatorio fue menor que 0.5.");
             } else {
-                resolve("✅ Promesa ejecutada con éxito después de 8 segundos!");
+                resolve("✅ Promesa ejecutada con éxito: número >= 0.5.");
             }
-        }, 8000);
+        }, 3000);
     });
 }
 
@@ -14,11 +20,7 @@ function descargarArchivoConError(hayError) {
 async function ejecutar() {
     try {
         console.log("⏳ Promesa ejecutándose...");
-        
-        
-        const hayError = true; // Aqui se cambia el valor de esta variable para probar ambos casos
-
-        const respuesta = await descargarArchivoConError(hayError);
+        const respuesta = await promesaRechazada();
         console.log(respuesta);
     } catch (error) {
         console.error(error);
